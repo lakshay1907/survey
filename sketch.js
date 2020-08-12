24e9f611b8c7a9d33e1b356a48887142ec2fe82d
lakshay1907 <64905086+lakshay1907@users.noreply.github.com>
2020-08-13 00:48:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,14 +20,14 @@ function draw(){
     fill(0);
     text("Enter Email",170,530);
     textSize(20);
-    
-   text("Q-1 Who was the first vice president of free India?",3,20);
+    text("Q-1 Who was the first vice president of free India?",3,20);
     text("Q-2 Who was the first to lead the Indian army?",3,80);
     text("Q-3 For how many years british ruled India?",3,160);
     text("Q-4 Who led the salt march?",3,245);
     text("Q-5 Which country celebrate it's Independance Day ",3,300);
     text("one day before of India?",39,320);
     text("Q-6 Are you celebrating Independance?",3,400);
+   
 }
 
 
